8805ae732209b0fd72e96cbd226d0f42521f4696
{maekawa-sho} <zhanumoukadogemubadito@gmail.com>
2023-08-11 17:36:05 +0900

    ranking

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from './components/Home';
 import { Page1 } from './components/gamepage';
+import  Mypage from './Mypage';
+import  Register  from "./Register";
+import  Login  from "./Login";
+import  Ranking  from "./components/ranking";
+
+
 import './App.css';
 
 function App() {
@@ -9,8 +14,11 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path='/' element={<Mypage />} />
           <Route path="/gamepage" element={<Page1 />} />
+          <Route path="/register/" element={<Register />} />
+          <Route path="/login/" element={<Login />} />
+          <Route path="/ranking" element={<Ranking />} />
         </Routes>
       </div>
     </BrowserRouter>
